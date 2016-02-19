f46f33e1a5c1ab6d3e210be0619736e387a75160
thenriquez <thenriquez25@outlook.com>
2016-02-19 17:40:45 -0500

    finsh it

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
+var GitHubApi = require("node-github");
+var github = new GitHubApi({
+      version: "3.0.0"
+});
+
 
 app.use ("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
@@ -16,6 +21,20 @@ app.get("/dashboard", function(){
   res.sendFile(process.cwd() + "/views/dashboard.html");
 
 });
+github.user.getFollowingFromUser({
+    user: "thenriuqez"
+}, function(err, res) {
+    console.log(JSON.stringify(res));
+});
+app.get('github/:user'), function(req , res){
+    var user = req.params.user;
+    github.user.getFollowingFromUser({
+        user: "thenriuqez"
+    }, function(err, res) {
+        console.log(JSON.stringify(res));
+    });
+  }
+
 
 app.listen(PORT, function(){
   console.log("Listening on port %s", PORT);
